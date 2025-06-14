5e83b7adf0de9c6c9b1aacccefb06aa778b38be8
Carlos <supernumerovirtual@gmail.com>
2025-06-13 23:39:26 -0400

    Create nsfw-pack.js

---
+++ plugins/nsfw-pack.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+
+    if (!db.data.chats[m.chat].nsfw && m.isGroup) {
+    return m.reply(hotw);
+    }
+
+    m.react('💋');
+
+    let txt = 'Pack 🔥';
+    let img = 'https://delirius-apiofc.vercel.app/nsfw/girls';
+
+    let buttons = [
+        {
+            buttonId: `.pack`,
+            buttonText: { displayText: "Ver más" },
+            type: 1
+        }
+    ];
+    await conn.sendMessage(
+        m.chat,
+        {
+            image: { url: img },
+            caption: txt,
+            buttons: buttons,
+            viewOnce: true
+        },
+        { quoted: m }
+    );
+};
+
+handler.command = ['pack'];
+handler.group = true;
+export default handler;
