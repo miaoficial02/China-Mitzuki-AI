f87f0588bd5d2d60a211a9c9319926ec86152ff2
Carlos <supernumerovirtual@gmail.com>
2025-07-03 14:24:41 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,51 +1,27 @@
-import { generarTarjeta } from '../lib/generarTarjeta.js'
-import moment from 'moment-timezone'
 import { WAMessageStubType } from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return
-  const chat = global.db.data.chats[m.chat]
-  if (!chat.welcome) return
-
-  if (m.messageStubType !== WAMessageStubType.ADD) return
-
-  const id = m.messageStubParameters[0]
-  const senderJid = id.includes('@') ? id : `${id}@s.whatsapp.net`
-
-  // Obtener foto de perfil o usar predeterminada
-  const avatarUrl = await conn.profilePictureUrl(senderJid, 'image')
-    .catch(_ => 'https://i.imgur.com/Oy2Stgq.png')
-
-  const nombre = `@${id.split('@')[0]}`
-  const numero = id.split('@')[0]
-  const fecha = moment().tz('America/Havana').format('DD/MM/YYYY HH:mm')
-  const miembros = participants.length + 1
-  const grupo = groupMetadata.subject
-
-  // Generar imagen
-  const bufferTarjeta = await generarTarjeta({
-    nombre,
-    numero,
-    grupo,
-    miembros,
-    avatarUrl,
-    fechaIngreso: fecha
-  })
-
-  // Mensaje citado
-  const fkontak = {
-    "key": { "participants": "0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" },
-    "message": {
-      "contactMessage": {
-        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Bot;Bienvenido;;;\nFN:Bienvenido\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Usuario\nEND:VCARD`
-      }
-    }
+  if (!m.messageStubType || !m.isGroup) return !0;
+  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
+  let img = await (await fetch(`${pp}`)).buffer()
+  let chat = global.db.data.chats[m.chat]
+  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
+  let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
+  let groupSize = participants.length
+  if (m.messageStubType == 27) {
+    groupSize++;
+  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
+    groupSize--;
   }
 
-  // Enviar tarjeta de bienvenida
-  await conn.sendMessage(m.chat, {
-    image: bufferTarjeta,
-    caption: `✨ *Bienvenid@ ${nombre} a ${grupo}* ✨\nActualmente somos ${miembros} miembros.\n🤖 Usa *#menu* para comenzar a interactuar con el bot.`,
-    mentions: [senderJid]
-  }, { quoted: fkontak })
-}
+  if (chat.welcome && m.messageStubType == 27) {
+    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
+  }
+  
+  if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
+    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
+  }}
