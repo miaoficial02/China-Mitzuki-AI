9083cce07a7d6ebae2fff160379ce292e5c921cf
Carlos <supernumerovirtual@gmail.com>
2025-07-05 08:26:31 -0400

    Actualizar anime-hug.js

--- plugins/anime-hug.js
+++ plugins/anime-hug.js
@@ -25,8 +25,7 @@ let handler = async (m, { conn }) => {
         `┃ ${name2} le dio un abrazo a ${name}\n` +
         `╰──────────────────────────────╯`
 
-  const firma = '\n\n— ✦ Código embellecido por Carlos ✦\n✧ github.com/Kone457/Shizuka-AI'
-  str += firma
+  
 
   if (m.isGroup) {
     const videos = [
