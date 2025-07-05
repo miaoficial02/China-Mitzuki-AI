e5dc75051d9e24734ab58b0e2b07b049fb1c6737
Carlos <supernumerovirtual@gmail.com>
2025-07-05 08:26:17 -0400

    Actualizar anime-kill.js

--- plugins/anime-kill.js
+++ plugins/anime-kill.js
@@ -25,8 +25,7 @@ let handler = async (m, { conn }) => {
         `┃ ${name2} eliminó a ${name} sin piedad\n` +
         `╰─────────────────────────────╯`
 
-  const firma = '\n\n— ✦ Código embellecido por Carlos ✦\n✧ github.com/Kone457/Shizuka-AI'
-  str += firma
+  
 
   if (m.isGroup) {
     const videos = [
