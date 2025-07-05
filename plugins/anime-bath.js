8749c6c18a5307946980df2757d52878b5930c5e
Carlos <supernumerovirtual@gmail.com>
2025-07-05 07:58:10 -0400

    Actualizar anime-bath.js

--- plugins/anime-bath.js
+++ plugins/anime-bath.js
@@ -1,47 +1,60 @@
-/* 
-❀ Codígo creado por Destroy
-✧ https://github.com/The-King-Destroy/Yuki_Suou-Bot.git 
-*/
-
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let who = m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
+  let who = m.mentionedJid.length > 0
+    ? m.mentionedJid[0]
+    : (m.quoted ? m.quoted.sender : m.sender);
 
-    let str = m.mentionedJid.length > 0 || m.quoted 
-        ? `\`${name2}\` está bañando a \`${name || who}\` ٩(ˊᗜˋ )و` 
-        : `\`${name2}\` se está bañando ٩(ˊᗜˋ )و`
-    
-    if (m.isGroup) {
-        let pp = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788344166.mp4'
-        let pp2 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788334922.mp4'
-        let pp3 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788381433.mp4'
-        let pp4 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788373764.mp4'
-        let pp5 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788367604.mp4'
-        let pp6 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788360468.mp4'
-        let pp7 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788351832.mp4'
-        let pp8 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788411685.mp4'
-        let pp9 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788405466.mp4'
-        let pp10 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788400374.mp4'
-        let pp11 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788391016.mp4'
-        let pp12 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788385627.mp4'
-        let pp13 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595152236.mp4'
-        let pp14 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595159911.mp4'
-        let pp15 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595155336.mp4'
-        
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15]
-        const video = videos[Math.floor(Math.random() * videos.length)]
-        
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
-    }
-}
+  let name = await conn.getName(who);
+  let name2 = await conn.getName(m.sender);
 
-handler.help = ['bath']
-handler.tags = ['anime']
-handler.command = ['bath', 'bañarse']
-handler.group = true
+  let str =
+    who === m.sender
+      ? `╭───〔 🧼 𝘽𝙖𝙣̃𝙤 𝙍𝙚𝙡𝙖𝙟𝙖𝙣𝙩𝙚 〕───╮\n` +
+        `┃ ${name2} se está dando un buen baño ٩(ˊᗜˋ*)و\n` +
+        `╰────────────────────────╯`
+      : `╭──〔 🛁 𝘽𝙖𝙣̃𝙤 𝘼𝙡𝙚𝙜𝙧𝙚 〕──╮\n` +
+        `┃ ${name2} está bañando a ${name || '@' + who.split('@')[0]} 🧴🫧\n` +
+        `╰────────────────────╯`;
 
-export default handler
+  if (m.isGroup) {
+    const videos = [
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788344166.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788334922.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788381433.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788373764.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788367604.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788360468.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788351832.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788411685.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788405466.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788400374.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788391016.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742788385627.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595152236.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595159911.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595155336.mp4'
+    ];
+
+    const video = videos[Math.floor(Math.random() * videos.length)];
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        video: { url: video },
+        gifPlayback: true,
+        caption: str,
+        mentions: [who]
+      },
+      { quoted: m }
+    );
+  }
+};
+
+handler.help = ['bath'];
+handler.tags = ['anime'];
+handler.command = ['bath', 'bañarse'];
+handler.group = true;
+
+export default handler;
