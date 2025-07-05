4c331c8b21a3b32eb25076543470faa29692ae42
Carlos <supernumerovirtual@gmail.com>
2025-07-05 14:11:24 -0400

    Actualizar A.js

--- plugins/A.js
+++ plugins/A.js
@@ -3,7 +3,7 @@
 
 import fetch from 'node-fetch';
 
-const handler = async (m, { conn }) => {
+const handler = async (m, { conn, usedPrefix: _p }) => {
   const response = await fetch('https://api.thecatapi.com/v1/images/search');
   const json = await response.json();
   const imageUrl = json[0]?.url;
@@ -11,13 +11,16 @@ const handler = async (m, { conn }) => {
   const message = {
     image: { url: imageUrl },
     caption:
-      '🌿 *Naturaleza Random*\n' +
-      'Aquí tienes una dosis de ternura o paisaje aleatorio.\n' +
-      '🎴 ¿Quieres otra imagen?\n',
-    footer: 'Plugin visual creado por Carlos',
+      '🌿 *Naturaleza Random*\n\n' +
+      'Una dosis aleatoria de ternura 🐾.\n' +
+      'Pulsa abajo para recibir otra más.',
+    footer: 'Plugin creado por Carlos • github.com/Kone457',
     buttons: [
-      { buttonId: '.catpic', buttonText: { displayText: '🔁 Otra imagen' }, type: 1 },
-      { buttonId: '.menu', buttonText: { displayText: '📜 Menú' }, type: 1 }
+      {
+        buttonId: `${_p}catpic`,
+        buttonText: { displayText: '🔁 Ｏ Ｔ Ｒ Ａ Ｍ Á Ｓ' },
+        type: 1
+      }
     ],
     headerType: 4
   };
