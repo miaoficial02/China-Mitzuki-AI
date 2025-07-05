45f1cae66e467ed96d8abaf54d58e21dc0afc210
Carlos <supernumerovirtual@gmail.com>
2025-07-05 08:04:00 -0400

    Actualizar anime-bite.js

--- plugins/anime-bite.js
+++ plugins/anime-bite.js
@@ -1,47 +1,60 @@
-/* 
-❀ Codígo creado por Destroy
-✧ https://github.com/The-King-Destroy/Yuki_Suou-Bot.git 
-*/
-
-import fs from 'fs'
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let who = m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
+  let who = m.mentionedJid.length > 0
+    ? m.mentionedJid[0]
+    : (m.quoted ? m.quoted.sender : m.sender);
 
-    let str = m.mentionedJid.length > 0 || m.quoted 
-        ? `\`${name2}\` mordió a \`${name || who}\` ≽^•⩊•^≼` 
-        : `\`${name2}\` se mordió a sí mismo/a ≽^•⩊•^≼`
-    
-    if (m.isGroup) {
-        let pp = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789640809.mp4'
-        let pp2 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789647443.mp4'
-        let pp3 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789653530.mp4'
-        let pp4 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789660235.mp4'
-        let pp5 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789666686.mp4'
-        let pp6 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789604757.mp4'
-        let pp7 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789611045.mp4'
-        let pp8 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789617091.mp4'
-        let pp9 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789623357.mp4'
-        let pp10 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789629746.mp4'
-        let pp11 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789592139.mp4'
-        let pp12 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789597656.mp4'
-        let pp13 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595529576.mp4'
-        let pp14 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595524733.mp4'
-        let pp15 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595520348.mp4'
-        
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15]
-        const video = videos[Math.floor(Math.random() * videos.length)]
-        
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
-    }
-}
+  let name = await conn.getName(who);
+  let name2 = await conn.getName(m.sender);
 
-handler.help = ['bite']
-handler.tags = ['anime']
-handler.command = ['bite', 'morder']
-handler.group = true
+  let str =
+    who === m.sender
+      ? `╭─〔 😖 𝘼𝙪𝙩𝙤-𝙈𝙤𝙧𝙙𝙞𝙙𝙖 〕─╮\n` +
+        `┃ ${name2} se mordió a sí mism@ ≽^•⩊•^≼\n` +
+        `╰────────────────────╯`
+      : `╭──〔 🐾 𝙈𝙤𝙧𝙙𝙞𝙙𝙖 𝙇𝙞𝙣𝙙𝙖 〕──╮\n` +
+        `┃ ${name2} mordió a ${name || '@' + who.split('@')[0]} ≽^•⩊•^≼\n` +
+        `╰────────────────────╯`;
 
-export default handler
+  if (m.isGroup) {
+    const videos = [
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789640809.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789647443.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789653530.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789660235.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789666686.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789604757.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789611045.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789617091.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789623357.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789629746.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789592139.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742789597656.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595529576.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595524733.mp4',
+      'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745595520348.mp4'
+    ];
+
+    const video = videos[Math.floor(Math.random() * videos.length)];
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        video: { url: video },
+        gifPlayback: true,
+        caption: str,
+        mentions: [who]
+      },
+      { quoted: m }
+    );
+  }
+};
+
+handler.help = ['bite'];
+handler.tags = ['anime'];
+handler.command = ['bite', 'morder'];
+handler.group = true;
+
+export default handler;
