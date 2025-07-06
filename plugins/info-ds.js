a10ab5a3a66b940f577d348087a4923967074cde
Carlos <supernumerovirtual@gmail.com>
2025-07-06 01:17:30 -0400

    Actualizar info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -10,11 +10,11 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, ${emoji} Utiliza este comando directamente en el número principal del Bot., m)
+return conn.reply(m.chat, `${emoji} Utiliza este comando directamente en el número principal del Bot.`, m)
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
-let sessionPath = ./${sessions}/
+let sessionPath = `./${sessions}/`
 
 try {
 
@@ -29,14 +29,14 @@ break
 }}}
 
 if (filesDeleted === 0) {
-await conn.reply(m.chat, ${emoji2} No se encontró ningún archivo que incluya la ID del chat., m)
+await conn.reply(m.chat, `${emoji2} No se encontró ningún archivo que incluya la ID del chat.`, m)
 } else {
-await conn.reply(m.chat, ${emoji2} Se eliminaron ${filesDeleted} archivos de sesión., m)
-conn.reply(m.chat, ${emoji} ¡Hola! ¿logras verme?, m)
+await conn.reply(m.chat, `${emoji2} Se eliminaron ${filesDeleted} archivos de sesión.`, m)
+conn.reply(m.chat, `${emoji} ¡Hola! ¿logras verme?`, m)
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
-await conn.reply(m.chat, ${emoji} Hola Soy ${botname} Sigue El Canal y apoyanos porfavor.\n\n> ${channel}, m)
+await conn.reply(m.chat, `${emoji} Hola Soy ${botname} Sigue El Canal y apoyanos porfavor.\n\n> ${channel}`, m)
 }
 
 }
