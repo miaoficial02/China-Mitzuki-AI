a03fa86a8d726394009885445dfcd37e8be18c15
Carlos <supernumerovirtual@gmail.com>
2025-07-23 13:32:13 -0400

    Actualizar owner-setname.js

--- plugins/owner-setname.js
+++ plugins/owner-setname.js
@@ -1,19 +1,34 @@
-let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona un nombre para el bot.\n> Ejemplo: #setname Nombre/Texto`);
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    return m.reply(
+      `🌙 *Debes indicar el nuevo nombre para Shizuka.*\n` +
+      `📝 *Formato sugerido:* #setname NombrePrincipal/TextoSecundario`
+    )
+  }
 
-  const names = text.split('/');
-  if (names.length !== 2) return m.reply(`${emoji} Por favor, proporciona ambos nombres separados por una barra (/) en el formato: nombre1/nombre2.`);
+  const names = text.split('/')
+  if (names.length !== 2) {
+    return m.reply(
+      `⚠️ *Formato incorrecto.*\n` +
+      `🌸 Por favor, usa una barra (/) para separar el nombre y el texto.\n` +
+      `📎 Ejemplo: Shizuka/Mi luz entre códigos`
+    )
+  }
 
-  global.botname = names[0].trim();
-  const texto1bot = ` • Powered By ${etiqueta}`;
-  global.textbot = `${names[1].trim()}${texto1bot}`;
-  
-  m.reply(`${emoji} El nombre del bot ha sido cambiado a: ${global.botname}\n\n> ${emoji2} El texto del bot ha sido cambiado a: ${global.textbot}`);
-};
+  global.botname = names[0].trim()
+  const texto1bot = ` • Powered By ${etiqueta}`
+  global.textbot = `${names[1].trim()}${texto1bot}`
 
-handler.help = ['setname'];
-handler.tags = ['tools'];
-handler.command = ['setname'];
-handler.rowner = true;
+  m.reply(
+    `🎐 *Transformación completada.*\n\n` +
+    `🪷 *Nombre principal cambiado a:* ${global.botname}\n` +
+    `📜 *Frase representativa actualizada a:*\n${global.textbot}`
+  )
+}
 
-export default handler;
+handler.help = ['setname']
+handler.tags = ['tools']
+handler.command = ['setname']
+handler.rowner = true
+
+export default handler
