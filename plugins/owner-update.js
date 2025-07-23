7f567ff2ed11e71452cabef9f84b4c8092f3fd1e
Carlos <supernumerovirtual@gmail.com>
2025-07-23 13:26:34 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,29 +1,37 @@
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
-  m.reply(`${emoji2} Actualizando el bot...`);
+  await m.reply(`🌸 *Shizuka está iniciando el proceso de actualización...*`)
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
-      return;
+      conn.reply(
+        m.chat,
+        `🌀 *Error detectado durante la actualización.*\n\n📎 Detalle técnico:\n${err.message}`,
+        m
+      )
+      return
     }
 
     if (stderr) {
-      console.warn('Advertencia durante la actualización:', stderr);
+      console.warn('🌬️ Advertencia durante la actualización:', stderr)
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `${emoji4} El bot ya está actualizado.`, m);
+      conn.reply(m.chat, `🧘‍♀️ *Todo está en calma.*\n✨ *Shizuka ya está actualizada.*`, m)
     } else {
-      conn.reply(m.chat, `${emoji} Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(
+        m.chat,
+        `🌕 *Actualización completada con éxito.*\n\n🔧 *Detalles del proceso:*\n${stdout}`,
+        m
+      )
     }
-  });
-};
+  })
+}
 
-handler.help = ['update'];
-handler.tags = ['owner'];
-handler.command = ['update'];
-handler.rowner = true;
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = ['update']
+handler.rowner = true
 
-export default handler;
+export default handler
