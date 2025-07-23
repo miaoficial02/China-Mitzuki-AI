550574f5c7bcf03747e115c1bb7fe01b4908dde4
Carlos <supernumerovirtual@gmail.com>
2025-07-23 13:27:33 -0400

    Actualizar owner-setstatus.js

--- plugins/owner-setstatus.js
+++ plugins/owner-setstatus.js
@@ -1,13 +1,25 @@
 let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa la nueva biografia que deseas ponerme.`, m)
-     try {
-                await conn.updateProfileStatus(text).catch(_ => _)
-                conn.reply(m.chat, `${emoji} Info Cambiada Con Exito...`, m)
-} catch {
-       throw 'Well, Error Sis...'
-     }
+  if (!text) {
+    return conn.reply(
+      m.chat,
+      `🌙 *Por favor, escribe la nueva biografía que deseas que Shizuka exprese.*`,
+      m
+    )
+  }
+
+  try {
+    await conn.updateProfileStatus(text).catch(_ => _)
+    await conn.reply(
+      m.chat,
+      `🌸 *Biografía actualizada con éxito.*\n🦢 *Shizuka ahora refleja tu nuevo mensaje interior.*`,
+      m
+    )
+  } catch (e) {
+    throw '🌀 *Hubo un pequeño error en el camino...*'
+  }
 }
-handler.help = ['setstatus <teks>']
+
+handler.help = ['setstatus <texto>']
 handler.tags = ['owner']
 handler.command = ['setstatus', 'setbio']
 handler.rowner = true
