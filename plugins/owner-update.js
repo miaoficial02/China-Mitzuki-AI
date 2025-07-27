97e32fdca1ec08bc4745d7ae8f809332eb71b166
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-07-26 22:21:52 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'up']
+handler.command = ['update']
 handler.rowner = true
 
 export default handler
