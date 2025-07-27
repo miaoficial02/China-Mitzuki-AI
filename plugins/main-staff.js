a7af21c256fcad4316eafe74ce5ecbb04fa455e2
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-07-27 18:31:46 -0400

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let img = './src/catalogo.jpg'
+let img = 'https://files.catbox.moe/hf662e.jpg'
 let staff = `
-✰ *Dueño* https://Wa.me/5355699866
+❐ *Dueño* https://Wa.me/18493907272
 ✦ *Bot:* ${botname}
-⚘ *Versión:* ${vs}
-❖ *Libreria:* ${libreria} ${baileys}
+❐ *Versión:* ${vs}
+❐ *Libreria:* ${libreria} ${baileys}
 
-❍ *Creador:*
+✎ *Creador:*
 
- ☬ 𝘾𝙖𝙧𝙡𝙤𝙨 ☬
-> 🜸 Rol » *Creador*
-> ✧ GitHub » https://github.com/Kone457
+ 𝑬𝒓𝒆𝒏𝒙𝒛𝒚 𝑿𝒛𝒚
+> ✎ Rol » *Creador*
+> ❐ GitHub » https://github.com/erenxzy
 
 `
 await conn.sendFile(m.chat, img, '', staff.trim(), fkontak)
