f688b6d3d98e15082e05acd731835d6a60c83379
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-07-26 21:50:40 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,28 +1,28 @@
 import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
-  await m.reply(`🌸 *Shizuka está iniciando el proceso de actualización...*`)
+  await m.reply(`🌨️ 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐧𝐝𝐨 𝐋𝐚 𝐍𝐢𝐞𝐛𝐥𝐚 𝐃𝐞𝐥 𝐈𝐧𝐟𝐢𝐧𝐢𝐭𝐨 ↻`)
 
   exec('git pull', (err, stdout, stderr) => {
     if (err) {
       conn.reply(
         m.chat,
-        `🌀 *Error detectado durante la actualización.*\n\n📎 Detalle técnico:\n${err.message}`,
+        ` 𝐄𝐫𝐫𝐨𝐫 𝐀𝐥 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐫 𝐋𝐚 𝐍𝐢𝐞𝐛𝐥𝐚 ❄️\n\n📎 Detalle técnico:\n${err.message}`,
         m
       )
       return
     }
 
     if (stderr) {
-      console.warn('🌬️ Advertencia durante la actualización:', stderr)
+      console.warn('🌨️ Advertencia durante la actualización:', stderr)
     }
 
     if (stdout.includes('Already up to date.')) {
-      conn.reply(m.chat, `🧘‍♀️ *Todo está en calma.*\n✨ *Shizuka ya está actualizada.*`, m)
+      conn.reply(m.chat, `💠 *𝐓𝐨𝐝𝐨 𝐁𝐢𝐞𝐧 𝐓𝐫𝐚𝐧𝐪𝐮𝐢*\n✨ 𝐘𝐚 𝐋𝐚 𝐍𝐢𝐞𝐛𝐥𝐚 𝐃𝐞𝐥 𝐈𝐧𝐟𝐢𝐧𝐢𝐭𝐨 𝐄𝐬𝐭𝐚 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐚 ❄️`, m)
     } else {
       conn.reply(
         m.chat,
-        `🌕 *Actualización completada con éxito.*\n\n🔧 *Detalles del proceso:*\n${stdout}`,
+        `🌠 *Actualización con éxito.*\n\n❄️ *Detalles del proceso:*\n${stdout}`,
         m
       )
     }
@@ -31,7 +31,7 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update']
+handler.command = ['update', 'up']
 handler.rowner = true
 
 export default handler
