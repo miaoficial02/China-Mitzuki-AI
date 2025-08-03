1245dc95129be1a797c3b890bba90f961b9f0719
𝙀𝙧𝙚𝙣𝙭𝙯𝙮 <diegolesimarcelino@gmail.com>
2025-08-03 18:32:14 -0400

    Actualizar _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -32,7 +32,7 @@ const comando = m.text.trim().split(' ')[0]
 // Mensajes aleatorios para comandos inexistentes
 
 //const frasesError = [
-`╭━━━〔 🌨️ RUKIA-BOT 〕━━━╮
+//`╭━━━〔 🌨️ RUKIA-BOT 〕━━━╮
 ┃ 《✦》El comando *${comando}* no se encuentra disponible.
 ┃ Consulta la lista de comandos con:
 ┃ ➪ *.help*
